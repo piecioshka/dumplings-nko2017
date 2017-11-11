5c8616a641b1f367b6389f7e01b6305394040aa0
Piotr Kowalski <piecioshka@gmail.com>
2017-11-11 10:13:11 +0100

    Setup project with vue-cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,21 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
-    extends: 'piecioshka',
-
-    // http://eslint.org/docs/user-guide/configuring#specifying-environments
-    env: {
-        es6: true,
-        // browser: true,
-        // node: true,
-        // commonjs: true,
-        // amd: true,
-        // jquery: true,
-        jasmine: true
-    },
-
-    // http://eslint.org/docs/rules/
-    rules: {},
-
-    // List of global variables.
-    globals: {}
-};
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
