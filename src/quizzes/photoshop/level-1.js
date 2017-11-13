29588169ec984238dc71070bbffb570c8e1c737e
Piotr Kowalski <piecioshka@gmail.com>
2017-11-13 02:04:10 +0100

    Add all questions for Photoshop quiz

--- src/quizzes/photoshop/level-1.js
+++ src/quizzes/photoshop/level-1.js
@@ -3,11 +3,22 @@ export default {
   categoryName: 'Photoshop',
   promo: '/static/images/quiz-banners/logo-photoshop.jpg',
   author: {
-    name: 'Kasia Grabowska',
+    name: 'Katarzyna Grabowska',
     email: 'katarzyna@grabowska.org',
     url: 'https://grabowska.org/'
   },
   questions: [
+    {
+      title: "Z jakich kolorów składa się zestawienie kolorystyczne rgb:",
+      hints: [],
+      answers: [
+        "żółty, zielony, niebieski",
+        "czerwony, zielony, czarny",
+        "czerwony, zielony, niebieski",
+        "różowy, granatowy, biały"
+      ],
+      correctAnswerIndex: 0
+    },
     {
       title: "Jakim klawiszem możemy włączyć szybką maskę?",
       hints: [],
@@ -16,6 +27,50 @@ export default {
         "A",
         "S",
         "M"
+      ],
+      correctAnswerIndex: 0
+    },
+    {
+      title: "Przy użyciu jakiego klawisza rysując otrzymamy prostą linie?",
+      hints: [],
+      answers: [
+        "ctrl",
+        "P",
+        "shift",
+        "alt"
+      ],
+      correctAnswerIndex: 0
+    },
+    {
+      title: "Jakiej maski nie ma w Photoshopie?",
+      hints: [],
+      answers: [
+        "maska iluzyjna",
+        "maska wektorowa",
+        "maska przycinająca",
+        "maska warstwy"
+      ],
+      correctAnswerIndex: 0
+    },
+    {
+      title: "Jaki skrót zmienia kolor pierwszoplanowy na kolor tła i odwrotnie?",
+      hints: [],
+      answers: [
+        "X",
+        "Z",
+        "T",
+        "P"
+      ],
+      correctAnswerIndex: 0
+    },
+    {
+      title: "Czy w Photoshopie można stworzyć animacje flash?",
+      hints: [],
+      answers: [
+        "tak",
+        "nie",
+        "można ale krótką",
+        "nie wiem"
       ],
       correctAnswerIndex: 0
     }
